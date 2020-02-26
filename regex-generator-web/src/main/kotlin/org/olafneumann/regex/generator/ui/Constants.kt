a026a64631f87fbd5546b45c860dabab6ef46368
Olaf Neumann <git-account@olafneumann.org>
2020-02-26 20:21:02 +0100

    Adjust ui code

---
+++ regex-generator-web/src/main/kotlin/org/olafneumann/regex/generator/ui/Constants.kt
@@ -0,0 +1,25 @@
+package org.olafneumann.regex.generator.ui
+
+const val CLASS_HIDDEN = "rg-hidden"
+const val CLASS_MATCH_ROW = "rg-match-row"
+const val CLASS_MATCH_ITEM = "rg-match-item"
+const val CLASS_ITEM_SELECTED = "rg-item-selected"
+const val CLASS_ITEM_NOT_AVAILABLE = "rg-item-not-available"
+
+const val ELEMENT_DIV = "div"
+
+const val EVENT_CLICK = "click"
+const val EVENT_INPUT = "input"
+
+const val ID_INPUT_ELEMENT = "rg_raw_input_text"
+const val ID_TEXT_DISPLAY = "rg_text_display"
+const val ID_RESULT_DISPLAY = "rg_result_display"
+const val ID_ROW_CONTAINER = "rg_row_container"
+const val ID_CONTAINER_INPUT = "rg_input_container"
+const val ID_CONTAINER_PATTERN_SELECTION = "rg_pattern_selection_container"
+const val ID_CONTAINER_RESULT = "rg_regex_result_container"
+const val ID_CHECK_ONLY_MATCHES = "rg_onlymatches"
+const val ID_CHECK_CASE_INSENSITIVE = "rg_caseinsensitive"
+const val ID_CHECK_DOT_MATCHES_LINE_BRAKES = "rg_dotmatcheslinebreakes"
+const val ID_CHECK_MULTILINE = "rg_dotmatcheslinebreakes"
+const val ID_BUTTON_COPY = "rg_button_copy"
--- regex-generator-web/src/main/kotlin/org/olafneumann/regex/generator/ui/HtmlPage.kt
+++ regex-generator-web/src/main/kotlin/org/olafneumann/regex/generator/ui/HtmlPage.kt
@@ -12,32 +12,6 @@ import kotlin.dom.clear
 import kotlin.dom.removeClass
 
 
-const val CLASS_HIDDEN = "rg-hidden"
-const val CLASS_MATCH_ROW = "rg-match-row"
-const val CLASS_MATCH_ITEM = "rg-match-item"
-const val CLASS_ITEM_SELECTED = "rg-item-selected"
-const val CLASS_ITEM_NOT_AVAILABLE = "rg-item-not-available"
-
-const val ELEMENT_DIV = "div"
-
-const val EVENT_CLICK = "click"
-const val EVENT_INPUT = "input"
-
-const val ID_INPUT_ELEMENT = "rg_raw_input_text"
-const val ID_TEXT_DISPLAY = "rg_text_display"
-const val ID_RESULT_DISPLAY = "rg_result_display"
-const val ID_ROW_CONTAINER = "rg_row_container"
-const val ID_CONTAINER_INPUT = "rg_input_container"
-const val ID_CONTAINER_PATTERN_SELECTION = "rg_pattern_selection_container"
-const val ID_CONTAINER_RESULT = "rg_regex_result_container"
-const val ID_CHECK_ONLY_MATCHES = "rg_onlymatches"
-const val ID_CHECK_CASE_INSENSITIVE = "rg_caseinsensitive"
-const val ID_CHECK_DOT_MATCHES_LINE_BRAKES = "rg_dotmatcheslinebreakes"
-const val ID_CHECK_MULTILINE = "rg_dotmatcheslinebreakes"
-const val ID_BUTTON_COPY = "rg_button_copy"
-
-private val Int.characterUnits: String get() = "${this}ch"
-
 class HtmlPage(
     private val presenter: DisplayContract.Presenter
 ) : DisplayContract.View {
@@ -56,6 +30,8 @@ class HtmlPage(
 
     private val recognizerMatchToRow = mutableMapOf<RecognizerMatch, Int>()
     private val recognizerMatchToElements = mutableMapOf<RecognizerMatch, HTMLDivElement>()
+
+    private val Int.characterUnits: String get() = "${this}ch"
 
     init {
         textInput.addEventListener(EVENT_INPUT, { presenter.onInputChanges(inputText) })
